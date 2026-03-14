fd106e99d88f05de4b1ad674614522d385f2adb8
dimitriinc <dimitriinc@proton.me>
2026-03-13 20:33:34 -0500

    misc

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Navigation from './Navigation'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  const connectionString = import.meta.env.VITE_DB_CONNECTION
+    const connectionString = import.meta.env.VITE_DB_CONNECTION
 
-
-  return (
-    <>
-      <h1>Blog</h1>
-      <p>{connectionString}</p>
-    </>
-  )
+    return (
+        <>
+            <h1>Blog</h1>
+            <Navigation />
+            <p>{connectionString}</p>
+        </>
+    )
 }
 
 export default App
---
+++ src/Navigation.jsx
@@ -0,0 +1,17 @@
+import './Navigation.css'
+
+function Navigation() {
+    return <>
+        <nav>
+            <ul>
+                <li>Being</li>
+                <li>Theory</li>
+                <li>Write-Ups</li>
+                <li>Course</li>
+                <li>Art</li>
+            </ul>
+        </nav>
+    </>
+}
+
+export default Navigation
